a90a63e51871f0f6f089aebd5a6ddfcdab866c90
Shantanu <kulkarni.shantanu4@gmail.com>
2023-05-07 20:27:47 +0530

    Initial commit for windows xp web version

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+import Taskbar from './components/taskbar/Taskbar'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Taskbar />
+      </div>
+    </>
+  )
+}
+
+export default App
